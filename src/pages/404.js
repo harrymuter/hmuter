a5e6eb80a90c4444ba155e620ea43a38d01df2f5
harrymuter <harry.muter95@gmail.com>
2022-09-15 16:14:29 +0100

    rework

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,14 +7,16 @@ export default ({ data }) => {
   return (
     <Layout>
       <h1 className="page-title">Well this is awkward...</h1>
-      <div className="section">
-        <h2 className="section-title">404 error - page not found</h2>
-        <div className="fzf-subheading">
-          <p>It looks like you might be lost. Try another page.</p>
+      <div className="section" style={{ textAlign: "center" }}>
+        <h2 className="section-title" style={{ alignItems: "center" }}>
+          404 error - page not found
+        </h2>
+        <p style={{ width: "100%", textAlign: "center" }}>
+          It looks like you might be lost. Try another page.
+        </p>
+        <div className="fzf-image">
+          <img src={john} alt="Lost John Travolta" />
         </div>
-      </div>
-      <div className="fzf-image">
-        <img src={john} alt="Lost John Travolta" />
       </div>
     </Layout>
   )
