2d6873eab2112f10bfdd193465d6d6f09244ce88
harrymuter <harry.muter95@gmail.com>
2022-09-19 16:22:39 +0100

    restructure

--- src/pages/my-work.js
+++ src/pages/my-work.js
@@ -18,13 +18,18 @@ export default ({ data }) => {
         <div className="section-title">
           <h2>Professional Portfolio</h2>
         </div>
-        <p>
-          Please find below a few examples of the professional projects that I
-          have worked on through a business I co-founded, called Epic
-          Applications. We build web applications for local businesses using{" "}
-          <a href="https://reactjs.org/">ReactJS</a> and Wordpress. Again, if
-          you would like to know more about the work on this website then{" "}
-          <a href="mailto:harry.muter95@gmail.com">get in contact</a>.
+        <p className="professional-text">
+          Please find a few examples of the professional projects that I have
+          worked on through a business I co-founded, called Epic Applications.
+          We build web applications for local businesses using{" "}
+          <a href="https://reactjs.org/">ReactJS</a> and the Gatsby site
+          generator. If you would like to know more about this work then feel
+          free to{" "}
+          <a href="mailto:harry.muter95@gmail.com">get in contact with me</a> or
+          reach out via LinkedIn / Twitter. If you are looking to build your own
+          website and would like to discuss how we might be able to assist you,
+          don't hesitate to contact{" "}
+          <a href="https://www.epicdev.co.uk/contact">Epic Dev</a>.
         </p>
         <div className="portfolio-container">
           <div className="portfolio-box">
@@ -49,20 +54,25 @@ export default ({ data }) => {
         <div className="section-title">
           <h2>Academic Portfolio</h2>
         </div>
-        <p>
-          In addition to my professional work, please find some academic work I
-          have completed during my postgraduate studies whilst at the University
-          of East Anglia.
+        <p className="professional-text">
+          In addition to my professional work, please find some of the academic
+          work I completed during my postgraduate studies whilst at the
+          University of East Anglia. The first paper is a review article
+          concerning the ethics of data science which was written as an
+          assessment piece for one of my Master's modules; and the second is a
+          comprehensive report I compiled for my Master's dissertation, for the
+          University of East Anglia, that outlines the steps I would take to
+          improve one of their existing systems.
         </p>
-        <div className="academic">
-          <div className="portfolio-container">
-            <div className="portfolio-box">
-              <a href={essay}>Data Mining Ethics Essay</a>
-            </div>
-            <div className="portfolio-box">
-              <a href={dissertation}>MSc Dissertation</a>
-            </div>
+        {/* <div className="academic"> */}
+        <div className="portfolio-container">
+          <div className="portfolio-box">
+            <a href={essay}>Data Mining Ethics Essay</a>
           </div>
+          <div className="portfolio-box">
+            <a href={dissertation}>MSc Dissertation</a>
+          </div>
+          {/* </div> */}
         </div>
       </div>
     </Layout>
